714e3f90130cfb25d1fd761468168f0d301bdd0d
sinduke <sinduke@outlook.com>
2025-05-15 16:51:57 +0800

    [Feature] 添加Welcome UI页面

--- AIChatCourse/Core/WelcomeView/WelcomeView.swift
+++ AIChatCourse/Core/WelcomeView/WelcomeView.swift
@@ -8,21 +8,68 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    @State var imageName: String = Constants.randomImage
     var body: some View {
         NavigationStack {
             VStack {
-                Text("Welcome")
-                    .frame(maxHeight: .infinity)
-                NavigationLink {
-                    OnboardingCompletedView()
-                } label: {
-                    Text("Get start".uppercased())
-                        .callToActionButton()
-                }
+                ImageLoaderView(urlString: imageName)
+                    .ignoresSafeArea()
+                
+                titleSection
+                    .padding(.top, 24)
+                
+                ctaButtonSection
+                    .padding(16)
+                
+                policySection
             }
-            .padding(16)
         }
     }
+    
+    private var titleSection: some View {
+        VStack {
+            Text("AI Chat 👍")
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            Text("YouTube @swiftfulThinking")
+                .font(.caption)
+                .foregroundStyle(.secondary)
+        }
+    }
+    
+    private var ctaButtonSection: some View {
+        VStack {
+            NavigationLink {
+                OnboardingCompletedView()
+            } label: {
+                Text("Get start".uppercased())
+                    .callToActionButton()
+            }
+            Text("Already have an Account? Sign In")
+                .underline()
+                .font(.body)
+                .padding(8)
+                .onTapGesture {
+                    print("点击了按钮")
+                }
+        }
+    }
+    
+    private var policySection: some View {
+        HStack(spacing: 8) {
+            Link(destination: URL(string: Constants.teamsOfServiceURLString)!) {
+                Text("Teams of service")
+            }
+            Circle()
+                .fill(.accent)
+                .frame(width: 4)
+            Link(destination: URL(string: Constants.privacyPolicyURLString)!) {
+                Text("Privacy policy")
+            }
+        }
+    }
+    
 }
 
 #Preview {
---
+++ AIChatCourse/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  AIChatCourse
+//
+//  Created by sinduke on 5/15/25.
+//
+
+import SwiftUI
+
+struct Constants {
+    static let randomImage: String = "https://picsum.photos/600/600"
+    static let privacyPolicyURLString: String = "https://apple.com"
+    static let teamsOfServiceURLString: String = "https://apple.com"
+}
