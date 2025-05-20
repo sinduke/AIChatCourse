b5e1e99395d814997566ee69ac62245a5b13515e
sinduke <sinduke@outlook.com>
2025-05-20 15:12:19 +0800

    [Feature] 添加SDWebImageSwiftUI的强制刷新的功能, 这里结束主要的UI部分, 接下来进行数据的处理

--- AIChatCourse/Components/Images/ImageLoaderView.swift
+++ AIChatCourse/Components/Images/ImageLoaderView.swift
@@ -12,15 +12,29 @@ struct ImageLoaderView: View {
     var urlString: String = Constants.randomImage
     var resizingMode: ContentMode = .fill
     var forceTransitionAnimation: Bool = false
+    var forceReload: Bool = false
     var body: some View {
         Rectangle()
             .opacity(0.001)
             .overlay {
-                WebImage(url: URL(string: urlString))
-                    .resizable()
-                    .indicator(.activity)
-                    .aspectRatio(contentMode: resizingMode)
-                    .allowsHitTesting(false)
+                WebImage(
+                    url: URL(string: urlString)
+//                    options: [.refreshCached, .retryFailed, .highPriority]
+                )
+                .resizable()
+                .indicator(.activity)
+                .aspectRatio(contentMode: resizingMode)
+                .allowsHitTesting(false)
+                .ifSatisfiedCondition(forceReload) { content in
+                    content
+                        .onAppear {
+                            // 可选：完全清除缓存后再加载
+                            if let url = URL(string: urlString) {
+                                let key = SDWebImageManager.shared.cacheKey(for: url)
+                                SDImageCache.shared.removeImage(forKey: key, cacheType: .all, completion: nil)
+                            }
+                        }
+                }
             }
             .clipped()
             .ifSatisfiedCondition(forceTransitionAnimation) { content in
