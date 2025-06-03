fb76906da9d42d9b58f349290f7615df17852956
sinduke <sinduke@outlook.com>
2025-06-03 12:05:02 +0800

    [Feature] add CustomModalView component And App store Rating View

---
+++ AIChatCourse/Components/Modals/CustomModalView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomModalView.swift
+//  AIChatCourse
+//
+//  Created by sinduke on 6/3/25.
+//
+
+import SwiftUI
+
+struct CustomModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomModalView()
+}
